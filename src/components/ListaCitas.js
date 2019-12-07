f6a897110c4d213ad58f83a9c8d5a406960b76c4
Jimmy <bruno_pier@hotmail.com>
2019-12-06 22:36:04 -0300

    Conditional messaje in ListaCitas.js - No hay citas - Administra las citas aqui -

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,22 +2,28 @@ import React from 'react';
 import Cita from './Cita'
 import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas, eliminarCita}) => (
-            <div className="card mt-2 py-5">
-                <div className="card-body">
-                    <h2 className="card-title text-center">Administra las citas</h2>
-                    <div className="lista-citas">
-                        {citas.map(cita => (
-                            <Cita
-                            key={cita.id}
-                            cita={cita}
-                            eliminarCita={eliminarCita}
-                            />
-                        ))}
+const ListaCitas = ({citas, eliminarCita}) => {
+
+        //Imprimir mensaje en base a si hay citas o no
+        const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aqui';
+return (
+                <div className="card mt-2 py-5">
+                    <div className="card-body">
+                        <h2 className="card-title text-center">{mensaje}</h2>
+                        <div className="lista-citas">
+                            {citas.map(cita => (
+                                <Cita
+                                key={cita.id}
+                                cita={cita}
+                                eliminarCita={eliminarCita}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+
+}
 
 ListaCitas.propTypes = {
     citas : PropTypes.array.isRequired,
